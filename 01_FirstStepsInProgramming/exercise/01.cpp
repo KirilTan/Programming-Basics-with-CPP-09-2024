ff3699391e7b6ea06938482baa989ffe4d12f103
kiril <kiriltanev@proton.me>
2024-09-26 13:55:58 +0300

    01 solution added

---
+++ 01_FirstStepsInProgramming/exercise/01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double usd;
+    cin >> usd;
+
+    double bgn = usd * 1.79549;
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    cout << bgn << endl;
+
+    return 0;
+}
