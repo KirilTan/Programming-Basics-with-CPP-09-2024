942ee5ca78125666208fae35a628332f2a642f72
kiril <kiriltanev@proton.me>
2024-09-30 17:36:15 +0300

    01 solution updated

--- 01_FirstStepsInProgramming/exercise/01.cpp
+++ 01_FirstStepsInProgramming/exercise/01.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
+    const double exchangeRate = 1.79549;
+
     double usd;
     cin >> usd;
 
-    double bgn = usd * 1.79549;
+    double bgn = usd * exchangeRate;
 
-    cout.setf(ios::fixed);
-    cout.precision(2);
+    // cout.setf(ios::fixed);
+    // cout.precision(2);
 
-    cout << bgn << endl;
+    cout << fixed << setprecision(2) << bgn << endl;
 
     return 0;
 }
