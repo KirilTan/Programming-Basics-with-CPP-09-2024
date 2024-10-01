8179fa0f8bc98e88f5bc0bbae46c2b1727a3bd09
kiril <kiriltanev@proton.me>
2024-10-01 20:37:13 +0300

    02 solution added

---
+++ 02_ConditionalStatements/lab/02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // User input
+    int a, b;
+    cin >> a >> b;
+
+    // Output the bigger number
+    if (a > b)
+        cout << a << endl;
+    else
+        cout << b << endl;
+
+    return 0;
+}
