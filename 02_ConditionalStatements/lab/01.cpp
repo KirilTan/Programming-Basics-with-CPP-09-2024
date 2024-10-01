db219c93f667a7556a7b25d82217666f53dce634
kiril <kiriltanev@proton.me>
2024-10-01 20:23:25 +0300

    01 solution added

---
+++ 02_ConditionalStatements/lab/01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // User input
+    float score;
+    cin >> score;
+
+    // Check if output is needed
+    if (score >= 5.5)
+    {
+        // Output grade
+        cout << "Excellent!" << endl;
+    }
+}
