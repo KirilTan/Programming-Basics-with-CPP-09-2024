afdc3be4743e8eec55758fced4e349b004436714
kiril <kiriltanev@proton.me>
2024-10-01 21:05:41 +0300

    04 solution added

---
+++ 02_ConditionalStatements/lab/04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Password to check against
+    string passwordNeeded = "s3cr3t!P@ssw0rd";
+
+    // User input
+    string userPassword;
+    cin >> userPassword;
+
+    // Check if the password matches the required one
+    if (userPassword == passwordNeeded)
+        cout << "Welcome";
+    else
+        cout << "Wrong password!";
+
+    return 0;
+}
